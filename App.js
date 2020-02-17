120b6d994fa16264525e15a497ca0b26344d7cce
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-02-17 20:54:30 +0500

    Added Navigation

--- App.js
+++ App.js
@@ -1,6 +1,31 @@
 import React from "react";
-import EvenList from "./EventList";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
 
-export default function App() {
-  return <EvenList />;
-}
+import EventForm from "./EventForm";
+import EvenList from "./EventList";
+import { YellowBox } from "react-native";
+
+YellowBox.ignoreWarnings([
+  "Warning: componentWillMount is deprecated",
+  "Warning: componentWillReceiveProps is deprecated"
+]);
+
+const MainNavigator = createStackNavigator({
+  list: {
+    screen: EvenList,
+    navigationOptions: () => ({
+      title: "Your Events"
+    })
+  },
+  form: {
+    screen: EventForm,
+    navigationOptions: () => ({
+      title: "Add an Event"
+    })
+  }
+});
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
--- EventList.js
+++ EventList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { FlatList, StyleSheet } from "react-native";
+import ActionButton from "react-native-action-button";
 
 import EventCard from "./EventCard.js";
 
@@ -33,15 +34,24 @@ class EvenList extends Component {
     this.setState({ events });
   }
 
+  handleAddEvent = () => {
+    this.props.navigation.navigate("form");
+  };
+
   render() {
-    return (
+    return [
       <FlatList
         style={styles.list}
         data={this.state.events}
         renderItem={({ item }) => <EventCard event={item} />}
         keyExtractor={item => item.id}
+      />,
+      <ActionButton
+        key="fab"
+        onPress={this.handleAddEvent}
+        buttonColor="rgba(231, 76, 60, 1)"
       />
-    );
+    ];
   }
 }
 
