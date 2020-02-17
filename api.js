5ac80d736c2fcae97d88daf60d5abf883acc9ac2
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-02-17 21:48:22 +0500

    Added formatDateTime function

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-import moment from 'moment';
+import moment from "moment";
 
 export function formatDate(dateString) {
   const parsed = moment(new Date(dateString));
@@ -7,15 +7,27 @@ export function formatDate(dateString) {
     return dateString;
   }
 
-  return parsed.format('D MMM YYYY');
+  return parsed.format("D MMM YYYY");
+}
+
+export function formatDateTime(dateString) {
+  const parsed = moment(new Date(dateString));
+
+  if (!parsed.isValid()) {
+    return dateString;
+  }
+
+  return parsed.format("H A on D MMM YYYY");
 }
 
 export function getCountdownParts(eventDate) {
-  const duration = moment.duration(moment(new Date(eventDate)).diff(new Date()));
+  const duration = moment.duration(
+    moment(new Date(eventDate)).diff(new Date())
+  );
   return {
-    days: parseInt(duration.as('days')),
-    hours: duration.get('hours'),
-    minutes: duration.get('minutes'),
-    seconds: duration.get('seconds'),
+    days: parseInt(duration.as("days")),
+    hours: duration.get("hours"),
+    minutes: duration.get("minutes"),
+    seconds: duration.get("seconds")
   };
 }
